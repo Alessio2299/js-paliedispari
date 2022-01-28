cb48bae127e8eb0fbbc33cdb0abadb589b82e0bf
Alessio Napoli <alessionapoli99@icloud.com>
2022-01-28 14:45:56 +0100

    End Exercise JSnack 1 "Palindroma"

---
+++ Jnasck1/main.js
@@ -0,0 +1,26 @@
+// Creo un prompt, dove l'utente deve inserire la parola o la frase da esaminare""
+let chosenWord = prompt("Inserisci la parola da esaminare").toLowerCase().replaceAll(" ", "");
+
+console.log(chosenWord)
+// Creo una funziona dove inverte la singola parola specificata nel prompt
+
+function wordPalindroma (chosenWord){
+
+  let word = "";
+
+  for (let i = chosenWord.length -1; i >= 0; i--){
+    let letter = chosenWord[i];
+    word += letter    
+    console.log(word)
+  }
+  return word
+}
+// Uso la funzione wordPalindroma creata in precedenza
+let palindroma = wordPalindroma(chosenWord);
+
+// Creo una condizione
+if ( palindroma == chosenWord){
+  alert("La parola che hai inserito è palindroma")
+} else{
+  alert("La parola che hai inserito non è palindroma")
+}
